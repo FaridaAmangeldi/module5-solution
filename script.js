cf637f38e27d2cda57975b286fa4650ec2841263
FaridaAmangeldi <Amangeldifarida2007@gmail.com>
2026-03-18 17:58:48 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+// Example functions for Module 4
+
+function sayHello(name) {
+  console.log("Hello " + name);
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+// Test the functions
+sayHello("Student");
+
+let result = multiply(5, 3);
+console.log("5 x 3 =", result);
+
+// Extra test
+console.log("Module 4 is working!");
